c4ace5c453dcc92f36af57c0fa464043264154e0
Syed Bakhtawar Fahim <bakhtawarfahim10@gmail.com>
2022-02-26 06:43:50 -0800

    Intial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import './App.css';
 function App() {
   return (
     <React.Fragment>
+   
       <AppNavbar />
       <Routes>
         <Route path='/' element={<Home />} />
--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -5,7 +5,7 @@ function Footer()
 {
     return (
         <React.Fragment>
-            <footer className="bg-light text-center">
+            <footer className="bg-light text-center fixed bottom">
                 <p>© 2022 Syed Bakhtawar Fahim</p>
             </footer>
         </React.Fragment>
